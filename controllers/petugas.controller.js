ebe27996fc446deba0bfa2883811da3a143699c3
ErrisaAuraHidayah <errisahidayah@gmail.com>
2022-02-24 21:26:54 +0700

    upload login dan token validation

--- controllers/petugas.controller.js
+++ controllers/petugas.controller.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const bcrypt = require('bcryptjs/dist/bcrypt')
 const db = require('../db')
 
 module.exports = {
@@ -84,6 +85,40 @@ module.exports = {
                 })
             }
         })
-    }  
+    },
+    login: (req,res) => {
+        const username = req.body.username
+        const password = req.body.password
+
+        if (!username || !password) res.status(402).json({ message: 'username dan password harus diisi'})
+
+        return db.query(`SELECT * FROM petugas WHERE username = ?`, username, (err, result)=>{
+            if(err) return res.status(500).json({err})
+            const user = result[0]
+
+            if(typeof user === 'undefined'){
+                res.status(401).json({message: "email atau password tidak ditemukan"})
+            }
+
+            if(!bcrypt.compareSync(password, user.password)){
+                res.status(401).json({message : "Email dan password tidak sesuai"})
+            }
+
+            //ini untuk mengecek sebagai admin atau petugas
+            if(result[0].level === 'admin'){
+                const secretAdmin = '#$%^*&$#$^'
+                const token = jwt.sign({data: user}, secretAdmin)
+
+                return res.json({message : "Hai Admin! Login Berhasil, silahkan menggunakan token dibawah ini untuk mengakses endpoint private lain", token})
+            }else if(result[0].level === 'petugas'){
+                const secretPetugas = '$^%#@$&*^%'
+                const token = jwt.sign({data:user}, secretPetugas)
+
+                return res.json({message : "Hai Petugas! Login Berhasil, silahkan menggunakan token dibawah ini untuk mengakses endpoint private lain", token})
+
+            }
+
+        })
+    }
 
 }
